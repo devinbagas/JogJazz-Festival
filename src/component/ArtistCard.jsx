96e5f1b75f8472c8bbeb4e6cc9e223fcb1c72551
Devin Bagas Pratama <2300016015@webmail.uad.ac.id>
2024-12-31 12:17:43 +0700

    Commit 3.2

--- src/component/ArtistCard.jsx
+++ src/component/ArtistCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ArtistCard = ({ artist, onClick }) => {
   return (
     <div
-      className="relative flex justify-center items-center rounded-xl overflow-hidden -ml-10 md:ml-0 w-72 h-96 sm:w-56 sm:h-72 md:w-64 md:h-80 lg:w-72 lg:h-96 transition-transform hover:scale-110 shadow-[0_3px_21px_rgba(255,255,255,0.7)] cursor-pointer group"
+      className="relative flex justify-center items-center rounded-xl overflow-hidden -ml-5 md:ml-0 w-72 h-96 sm:w-56 sm:h-72 md:w-64 md:h-80 lg:w-72 lg:h-96 transition-transform hover:scale-110 shadow-[0_3px_21px_rgba(255,255,255,0.7)] cursor-pointer group"
       onClick={() => onClick(artist)}
     >
       {/* Image */}
